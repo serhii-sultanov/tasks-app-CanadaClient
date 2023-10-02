e4120784b4352b04f4e12774b1caa8e624772463
DmitryMass <dimamikron41@gmail.com>
2023-10-02 12:26:03 +0300

    profile fully completed

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,7 +1,8 @@
 import { ProfileEditForm } from '@/components/profile/ProfileEditForm';
 import { Reminder } from '@/components/profile/Reminder';
 import { UpdatePassword } from '@/components/profile/UpdatePassword';
-import type { TUserProfile } from '@/types/types';
+import { Loader } from '@/components/ui/Loader';
+import type { TTaskReminder, TUserProfile } from '@/types/types';
 import axios from 'axios';
 import { GetServerSideProps } from 'next';
 import { getSession } from 'next-auth/react';
@@ -9,6 +10,10 @@ import { FC } from 'react';
 
 type TProfileProps = {
   data: TUserProfile | null;
+  reminder?: TTaskReminder[] | null;
+};
+type TReminderProps = {
+  data: TTaskReminder[];
 };
 
 export const getServerSideProps: GetServerSideProps<TProfileProps> = async (
@@ -24,52 +29,66 @@ export const getServerSideProps: GetServerSideProps<TProfileProps> = async (
         },
       },
     );
-
     const data = response.data;
+
+    if (session?.user.role === 'client') {
+      return {
+        props: {
+          data,
+        },
+      };
+    }
+
+    const reminder: TReminderProps = await axios.get(
+      `${process.env.NEXT_PUBLIC_DATABASE_URL}/admin/taskReminder`,
+      {
+        headers: {
+          Authorization: `Bearer ${session?.user.token}`,
+        },
+      },
+    );
+
     return {
       props: {
         data,
+        reminder: reminder.data,
       },
     };
   } catch (err) {
     return {
-      props: { data: null },
+      props: { data: null, reminder: null },
     };
   }
 };
 
-const Profile: FC<TProfileProps> = ({ data }) => {
+const Profile: FC<TProfileProps> = ({ data, reminder }) => {
   if (!data) {
-    return (
-      <div className="text-black">
-        Empty or invalid data. Try to reload page.
-      </div>
-    );
+    return <Loader />;
   }
   const { businessName, firstName, lastName, email } = data;
 
   return (
     <div className="pb-10 pt-5">
-      <div className="flex gap-4 items-center max-lg:justify-center mb-8 pb-8 border-b border-grayStroke-100 border-opacity-20">
-        <div className="w-24 h-24 rounded-full bg-mainBLue flex justify-center items-center uppercase text-4xl font-semibold">
+      <div className="flex gap-4 items-center mb-8 pb-8 border-b border-grayStroke-100 border-opacity-20">
+        <div className="min-w-profileDesktop min-h-profileDesktop rounded-full bg-mainBLue flex justify-center items-center uppercase text-4xl font-semibold max-sm:text-lg max-sm:min-w-profileMobile max-sm:min-h-profileMobile">
           {firstName || lastName
             ? `${firstName.slice(0, 1)}${lastName.slice(0, 1)}`
             : email.slice(0, 2)}
         </div>
         <div>
-          <h3 className="text-black font-semibold text-3xl">
+          <h3 className="text-black font-semibold text-3xl max-sm:text-lg">
             {firstName} {lastName}
           </h3>
-          <h4 className="text-grayStroke-100 font-medium text-lg">
+          <h4 className="text-grayStroke-100 font-medium text-lg max-sm:text-sm16">
             {businessName}
           </h4>
-          <p className="text-grayStroke-70">{email}</p>
+          <p className="text-grayStroke-70 max-sm:text-s14">{email}</p>
         </div>
       </div>
       <ProfileEditForm profile={data} />
       <div className="flex justify-between items-center gap-8 max-sm:flex-col max-sm:items-stretch">
         <UpdatePassword />
-        <Reminder />
+        {reminder ? <Reminder reminder={reminder} /> : null}
       </div>
     </div>
   );
